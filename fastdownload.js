0480c273b926eca80a46fc27cea4c05acf29592d
Jimmy Hu <jim2212001@gmail.com>
2014-05-15 16:39:29 +0800

    make a new link for direct download

--- fastdownload.js
+++ fastdownload.js
@@ -1,27 +1,32 @@
 var file_selectors = ['a[id$="_lnkFile"]', 'a[id$="_linkFilePreview"]'];
 var disable_download_popup = function(){
-	for(var i=0; i<file_selectors.length; ++i)
-		disable_download_popup_sel(file_selectors[i]);
+  for(var i=0; i<file_selectors.length; ++i)
+    disable_download_popup_sel(file_selectors[i]);
 };
 var disable_download_popup_sel = function(selector){
-	var links = $(selector);
-	$.each(links, function(){
-		var obj = $(this);
-		var data = obj.attr('onclick');
-		//console.log(data);
-		if(!data || !data.length || !data.match(/AttachMediaId/))return;
-		var ids = ['AttachMediaId', 'CourseId'];
-		var url = 'common_get_content_media_attach_file.ashx?StudyLog=1';
-		for(var i=0; i<ids.length; ++i){
+  var links = $(selector);
+  $.each(links, function(){
+    var obj = $(this);
+    var data = obj.attr('onclick');
+    //console.log(data);
+    if(!data || !data.length || !data.match(/AttachMediaId/))return;
+    var ids = ['AttachMediaId', 'CourseId'];
+    var url = 'common_get_content_media_attach_file.ashx?StudyLog=1';
+    for(var i=0; i<ids.length; ++i){
       var id_regex = new RegExp('(' + ids[i] + '\=[a-z0-9\-]+)', 'gi');
       var extract_param = id_regex.exec(data)[1];
-			url += '&' + extract_param;
-		}
-		obj.attr('href', url);
-		obj.attr('onclick', null);
+      url += '&' + extract_param;
+    }
+    obj.attr('href', url);
 
-		preview_link(obj);
-	});
+    // Make new object without onclick
+    obj.removeAttr('onclick');
+    var newObj = obj.clone();
+    obj.after(newObj);
+    obj.remove()
+
+    preview_link(newObj);
+  });
 }
 var preview_link = function(obj){
   var preview_db = [
@@ -41,9 +46,13 @@ var preview_link = function(obj){
     break;
   }
 }
+
+
 disable_download_popup();
+setTimeout(disable_download_popup, 1000);
 var b = document.getElementsByTagName('body')[0];
 var observer = new window.MutationObserver(function(mutationRecord, observer){
-	disable_download_popup();
+  disable_download_popup();
 });
 observer.observe(b, {childList:true, subtree:true});
+
